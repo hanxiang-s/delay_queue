313bd24fd4d5923ee1fa009f289b0785c007a552
hanxiang <han_xiang@topsec.com.cn>
2025-07-14 13:27:16 +0800

    refactor: add note

--- pkg/action.go
+++ pkg/action.go
@@ -1,7 +1,7 @@
 package pkg
 
-type BaseAction interface {
-	ID() string
-	Cron() string
-	Execute(arg any) error
+type JobBaseAction interface {
+	ID() string            //任务ID
+	Cron() string          //任务定时表达式，表示多久执行一次从redis zset中获取0<score<当前秒时间戳member
+	Execute(arg any) error //任务执行方法
 }
